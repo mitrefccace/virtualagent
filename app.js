fd5f2d243abd0786dc8a383a7bc4df797ce9870c
Mike Woodman <mwoodman@mitre.org>
2017-08-23 11:59:33 -0400

    MySql connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+var config = require('./private/config.json');
 var express = require('express');
 var path = require('path');
-//var logger = require('morgan');
+var mysql = require('mysql');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -8,27 +9,58 @@ var index = require('./routes/index');
 
 var app = express();
 
+// Create MySQL connection and connect to the database
+var connection = mysql.createConnection({
+  host: config.mysql.host,
+  port: config.mysql.port,
+  user: config.mysql.user,
+  password: config.mysql.password,
+  database: config.mysql.database
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.log("MySQL Connection Error");
+  } else {
+    console.log("Connected to MySQL");
+  }
+});
+
+// Keeps connection from Inactivity Timeout
+setInterval(function () {
+  connection.ping();
+}, 60000);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Make the db accessible to routes
+app.use(function(req,res,next){
+    req.mysql = connection;
+    next();
+});
 
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
