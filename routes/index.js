6f07e4e03355df100266b07fcf3a531eaedd54a8
Mike Woodman <mwoodman@mitre.org>
2017-08-25 13:51:27 -0400

    adding data to upload

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ router.post('/uploadFile', function (req, res) {
       res.write('an error occurred');
     } else {
 
-      console.log("CallData: " + fields);
+      console.log("CallData: " + JSON.stringify(fields));
       /* Example files
       {
         "file":{
