57b76df3a49e9b2a37e35b215d6e86c82e8ae92d
Mike Woodman <mwoodman@mitre.org>
2017-09-21 13:19:15 -0400

    Update STUN

--- sockets.js
+++ sockets.js
@@ -13,7 +13,7 @@ function Socket(io) {
                 "ws": "wss://njsip.task3acrdemo.com/ws",
                 "sipUri": "sip:"+extension+"@njsip.task3acrdemo.com",
                 "pw": "1qaz1qaz",
-                "stun": "stun:newstun.task3acrdemo.com:3478"
+                "stun": "stun:stun.task3acrdemo.com:3478"
             };
             io.to(amiListener).emit('QueueAdd', {'extension': extension});
             io.to(socket.id).emit('registerJssip', jssipData);
